936d9041546f4fcd0de1212bd1a5aade04c6b4f8
Syed Tanveer Shah <syedtanveershahbacha111@gmail.com>
2026-08-06 10:58:13 +0500

    Add create admin command

--- core/settings.py
+++ core/settings.py
@@ -86,10 +86,14 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3002",
-    "http://localhost:3003",
-    "http://localhost:3000",
-    "http://localhost:3001",
+    'http://localhost:3002',
+    'http://localhost:3000',
+    'https://gro-nav-frontend.vercel.app',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://gro-nav-frontend.vercel.app',
+    'https://gronav-backend.onrender.com',
 ]
 
 ROOT_URLCONF = 'core.urls'
---
+++ src/api/management/commands/create_admin.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@gronav.com', 
+                password='Admin@123'
+            )
+            self.stdout.write('Superuser created!')
